c44dbcf89e1a54537e221421a09a41f9eeacebac
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-04-29 15:19:24 +0100

    implementing the project code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'celsius-kelvin';
+
+  c!: number | null;
+  f!: number | null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.c = 0;
+    this.f = this.convertToFahrenheit(this.c.toString());
+  }
+
+  // listening to keyup event
+  onKeyupCinput = (value: string) => this.f = this.convertToFahrenheit(value);
+
+  onKeyupFinput = (value: string) => this.c = this.convertToCelsius(value);
+
+  // converting methods. Formula: (10°C × 9/5) + 32 = 50°F
+  convertToFahrenheit(celsiusValue: string): number | null {
+    if (!celsiusValue) return null;
+    const cNumber = parseInt(celsiusValue, 10);
+    return +((cNumber * 9 / 5) + 32).toFixed(1);
+  }
+
+  convertToCelsius(fahrenheitValue: string): number | null {
+    if (!fahrenheitValue) return null;
+    const fNumber = parseInt(fahrenheitValue, 10);
+    return +((fNumber - 32) * 5 / 9).toFixed(1);
+  }
+
 }
